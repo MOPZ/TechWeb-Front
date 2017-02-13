aeaa311824b120cb490992af4380c39efff38fbd
MOPZ <paul.maupas@gmail.com>
2017-02-13 19:30:33 +0100

    Fix

--- js/characters.js
+++ js/characters.js
@@ -10,38 +10,50 @@ var view = new Vue({
 	},
 	methods: {
 		getAllCharacters:function(){
-            var url = 'http://localhost:3000/characters';
-            this.$http.get(url).then(function(response){
+			var url = 'http://localhost:3000/characters';
+			this.$http.get(url).then(function(response){
                 //Success
                 this.characters = response.body.characters;
-				var url2 = 'http://localhost:3000/users';
+                var url2 = 'http://localhost:3000/users';
 
-            		this.$http.get(url2).then(function(response){
-            			for (var i = this.characters.length - 1; i >= 0; i--) {
-            				for (var j = response.body.users.length - 1; j >= 0; j--) {
-            					if(this.characters[i].user_id == response.body.users[j].id)
-            						this.characters[i].user_id = response.body.users[j].name;
-            				};
-            			};
-                	}, function(response){
-                		//Failure
-           			 });
-
+                this.$http.get(url2).then(function(response){
+                	for (var i = this.characters.length - 1; i >= 0; i--) {
+                		for (var j = response.body.users.length - 1; j >= 0; j--) {
+                			if(this.characters[i].user_id == response.body.users[j].id)
+                				this.characters[i].user_id = response.body.users[j].name;
+                		};
+                	};
                 }, function(response){
+                		//Failure
+                	});
+
+            }, function(response){
                 //Failure
             });
-        },
-        getCharById:function(selectedId){
-	        var url = 'http://localhost:3000/characters/' + selectedId;
-	        this.$http.get(url).then(function(response){
+		},
+		getCharById:function(selectedId){
+			var url = 'http://localhost:3000/characters/' + selectedId;
+			this.$http.get(url).then(function(response){
 	        	//SUCCESS
 	        	this.selectedChar = response.body.character;
 	        	this.selectedCharPosX = this.selectedChar.position.x;
 	        	this.selectedCharPosY = this.selectedChar.position.y;
 
+	        	var url2 = 'http://localhost:3000/users';
+
+	        	this.$http.get(url2).then(function(response){
+	        		for (var i = response.body.users.length - 1; i >= 0; i--) {
+	        				if(this.selectedChar.user_id == response.body.users[i].id)
+	        					this.selectedChar.user_id = response.body.users[i].name;
+	        		
+	        		};
+	        	}, function(response){
+                		//Failure
+                });
+
 	        },function(response){
 	        	//FAILURE
 	        });
-        }
+		}
 	}
 });
--- js/users.js
+++ js/users.js
@@ -2,31 +2,59 @@ var view = new Vue({
 	el: "#vue-users",
 
 	data: {
-		selectedId:0,
+		selectedId:1,
 		users:{},
 		selectedUser:{},
 	},
 	methods: {
 		getAllUsers:function(){
-            var url = 'http://localhost:3000/users';
-            this.$http.get(url).then(function(response){
+			var url = 'http://localhost:3000/users';
+			this.$http.get(url).then(function(response){
                 //Success
                 console.log(response.body);
                 this.users = response.body.users;
+
+                var url2 = 'http://localhost:3000/alliances';
+
+                this.$http.get(url2).then(function(response){
+                	for (var i = this.users.length - 1; i >= 0; i--) {
+                		for (var j = response.body.alliances.length - 1; j >= 0; j--) {
+                			if(this.users[i].alliance_id == response.body.alliances[j].id)
+                				this.users[i].alliance_id = response.body.alliances[j].name;
+                		};
+                	};
                 }, function(response){
+                		//Failure
+                	});
+
+
+
+
+            }, function(response){
                 //Failure
             });
-        },
-        getUserById:function(selectedId){
-	        var url = 'http://localhost:3000/users/' + selectedId;
-	        this.$http.get(url).then(function(response){
+		},
+		getUserById:function(selectedId){
+			var url = 'http://localhost:3000/users/' + selectedId;
+			this.$http.get(url).then(function(response){
 	        	//SUCCESS
-	        	console.log(response.body);
 	        	this.selectedUser = response.body.user;
+	        	var url2 = 'http://localhost:3000/alliances';
+
+	        	this.$http.get(url2).then(function(response){
+	        		for (var i = response.body.alliances.length - 1; i >= 0; i--) {
+	        				if(this.selectedUser.alliance_id == response.body.alliances[i].id)
+	        					this.selectedUser.alliance_id = response.body.alliances[i].name;
+	        		
+	        		};
+	        	}, function(response){
+                		//Failure
+                });
+
 	        },function(response){
 	        	//FAILURE
 	        });
-        }
+		}
 	}
 });
 
